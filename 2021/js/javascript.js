0bc7aa45370692a9b2881a18225fde7a3a623828
Work <lightxaoc@mail.ru>
2021-06-23 17:38:22 +0200

    2 commit

--- 2021/js/javascript.js
+++ 2021/js/javascript.js
@@ -22,23 +22,55 @@ console.log(object.age);
 
 /*   ------------012--------------     */
 
-const numberOfFilms = +prompt('Crjkmrj фильмов вы уже посмотрели?', '');
+// const numberOfFilms = +prompt('Crjkmrj фильмов вы уже посмотрели?', '');
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: {},
-    privat: false
-};
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: {},
+//     privat: false
+// };
 
-const a = prompt('Один из последних фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+// const a = prompt('Один из последних фильмов?', ''),
+//       b = prompt('На сколько оцените его?', ''),
+//       c = prompt('Один из последних фильмов?', ''),
+//       d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
+
+/*   ------------013--------------     */
+
+const num = 50;
+
+// if(num < 49) {
+//     console.log('Error');
+// } else if (num > 100) {
+//     console.log('Много');
+// } else {
+//     console.log('OK!');
+// }
+
+// (num == 50) ? console.log('OK!') : console.log('Error');
+
+// switch (num) {
+//     case 49:
+//         console.log('Неверно');
+//         break;
+//     case 100:
+//         console.log('Неверно');
+//         break;
+//     case 50:
+//         console.log('Верно');
+//         break;    
+//     default:
+//         console.log('Не в этот раз');
+//         break;
+// }
+
+/*   ------------013--------------     */
+
 
