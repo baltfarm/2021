0a96f4b56f6f6d8a89bb57f1fc51200654d5dc68
Work <lightxaoc@mail.ru>
2021-07-07 14:04:48 +0200

    070721_002

--- project_01/js/script.js
+++ project_01/js/script.js
@@ -26,16 +26,30 @@ const movieDB = {
     ]
 };
 
-
-console.log(advert);
 advert.forEach((element, i) => {   
     if (i !== 0) { 
-    console.log(element); 
-
+    element.remove();
+    // console.log(element); 
     }  
 });
 
+const movieGenre = document.querySelector('.promo__genre');
+//movieGenre.innerHTML = 'Драмма';
+movieGenre.textContent = 'Драма';
+//console.log(movieGenre);
 
+const movieBG = document.querySelector('.promo__bg');
+movieBG.style.backgroundImage = 'url(../img/bg.jpg)';
 
+const movieItem = document.querySelectorAll('.promo__interactive-item');
+console.log(movieItem[1]);
+console.log(movieDB.movies[1]);
+movieDB.movies.sort();
+movieItem.forEach((element, i) => {
+    element.textContent = movieDB.movies[i];
+})
 
-
+const ulNumber = document.querySelectorAll('.promo__interactive-item');
+ulNumber.forEach((element, i) => {
+    element.textContent = i+1 + '. ' + element.textContent;
+})
