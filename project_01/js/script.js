3dc1f61ee163a35593754995c7ac103319685b43
Work <lightxaoc@mail.ru>
2021-07-14 12:03:53 +0200

    140721

--- project_01/js/script.js
+++ project_01/js/script.js
@@ -12,7 +12,7 @@
 
 5) Добавить нумерацию выведенных фильмов */
 
-'use strict';
+ 'use strict';
 
 
 
@@ -26,45 +26,115 @@ const movieDB = {
     ]
 };
 
-// const advert = document.querySelectorAll('img');
+// // const advert = document.querySelectorAll('img');
 
-// advert.forEach((element, i) => {   
-//     if (i !== 0) { 
-//     element.remove();
-//     // console.log(element); 
-//     }  
-// });
+// // advert.forEach((element, i) => {   
+// //     if (i !== 0) { 
+// //     element.remove();
+// //     // console.log(element); 
+// //     }  
+// // });
 
-const advert = document.querySelectorAll(".promo__adv img");
-advert.forEach(item => {
-    item.remove();
-})
+// const advert = document.querySelectorAll(".promo__adv img");
+// advert.forEach(item => {
+//     item.remove();
+// })
 
-const movieGenre = document.querySelector('.promo__genre');
-//movieGenre.innerHTML = 'Драмма';
-movieGenre.textContent = 'Драма';
-//console.log(movieGenre);
+// const movieGenre = document.querySelector('.promo__genre');
+// //movieGenre.innerHTML = 'Драмма';
+// movieGenre.textContent = 'Драма';
+// //console.log(movieGenre);
 
-const movieBG = document.querySelector('.promo__bg');
-// movieBG.style.backgroundImage = 'url(../img/bg.jpg)';
-movieBG.style.backgroundImage = "url('/img/bg.jpg')";
+// const movieBG = document.querySelector('.promo__bg');
+// // movieBG.style.backgroundImage = 'url(../img/bg.jpg)';
+// movieBG.style.backgroundImage = "url('/img/bg.jpg')";
 
-// const movieItem = document.querySelectorAll('.promo__interactive-item');
-// console.log(movieItem[1]);
-// console.log(movieDB.movies[1]);
+// // const movieItem = document.querySelectorAll('.promo__interactive-item');
+// // console.log(movieItem[1]);
+// // console.log(movieDB.movies[1]);
+// // movieDB.movies.sort();
+// // movieItem.forEach((element, i) => {
+// //     element.textContent = movieDB.movies[i];
+// // })
+
+// // const ulNumber = document.querySelectorAll('.promo__interactive-item');
+// // ulNumber.forEach((element, i) => {
+// //     element.textContent = i+1 + '. ' + element.textContent;
+// // })
+
+// const movieItem = document.querySelector(".promo__interactive-list");
+// movieItem.innerHTML = "";
 // movieDB.movies.sort();
-// movieItem.forEach((element, i) => {
-//     element.textContent = movieDB.movies[i];
+// movieDB.movies.forEach((item, i) => {
+// movieItem.innerHTML += `<li class="promo__interactive-item">${i+1}. ${item}</li>`;
 // })
 
-// const ulNumber = document.querySelectorAll('.promo__interactive-item');
-// ulNumber.forEach((element, i) => {
-//     element.textContent = i+1 + '. ' + element.textContent;
-// })
+/* Задания на урок:
 
-const movieItem = document.querySelector(".promo__interactive-list");
-movieItem.innerHTML = "";
-movieDB.movies.sort();
-movieDB.movies.forEach((item, i) => {
-movieItem.innerHTML += `<li class="promo__interactive-item">${i+1}. ${item}</li>`;
-})
+1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+новый фильм добавляется в список. Страница не должна перезагружаться.
+Новый фильм должен добавляться в movieDB.movies.
+Для получения доступа к значению input - обращаемся к нему как input.value;
+P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
+
+2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
+
+3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
+
+4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+"Добавляем любимый фильм"
+
+5) Фильмы должны быть отсортированы по алфавиту */
+
+// 'use strict';
+
+// Возьмите свой код из предыдущей практики
+window.addEventListener('DOMContentLoaded', () => {
+
+const btn = document.querySelector("button"),
+      formMovie = document.querySelector('form.add'),
+      newMovie = document.querySelector('.promo__interactive-list'),
+    //   deleteMovie = document.querySelectorAll('.delete'),
+      addMovie = formMovie.querySelector('.adding__input'),
+      checkbox = formMovie.querySelector('[type="checkbox"]');
+          
+
+  
+        
+    function newList(newMovies, dBase) {
+        newMovies.innerHTML = "";
+        dBase.forEach((item, i) => {
+            newMovies.innerHTML += `
+            <li class="promo__interactive-item">${i+1} ${item}
+                <div class="delete"></div>
+            </li>
+            `
+        });
+
+        document.querySelectorAll('.delete').forEach((item, i) => {
+            console.log('Work2');
+            item.addEventListener('click', (event) => {
+                console.log('Work');
+                event.preventDefault();
+                delete movieDB.movies[i];
+                console.log('Work');
+                 newList(newMovie, movieDB.movies);
+            });
+        }); 
+    };
+
+    formMovie.addEventListener('submit', (event) => {
+        event.preventDefault();
+        if (addMovie.value) {
+            if (addMovie.value.length > 21) {            
+                addMovie.value = addMovie.value.slice(0, 21) + ' ...';
+            }
+            movieDB.movies.push(addMovie.value);
+            movieDB.movies.sort();        
+            newList(newMovie, movieDB.movies);
+        //addMovie.value = "";
+        //formMovie.reset();
+            event.target.reset();
+        }
+    });
+});
